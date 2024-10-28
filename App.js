bedde214e3b3abfa013f3b1da38e9f67281e89cc
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-28 14:49:22 -0700

    finish 3.5

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import express from "express"
+import cors from "cors"
 import { Hello } from "./Hello.js"
 import { Lab5 } from "./lab5/index.js"
+
 const app = express()
+app.use(cors())
+app.use(express.json())
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
