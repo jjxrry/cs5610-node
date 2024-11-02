de1bfcb978a77e6996cee4a4df5758048feab770
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-11-02 14:20:07 -0700

    finish 4.1

---
+++ kanbas/database/assignments.js
@@ -0,0 +1,192 @@
+export default [
+    {
+        "_id": "A101",
+        "title": "Propulsion Assignment",
+        "course": "RS101",
+        "description": "Propulsion Assignment placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "Online",
+        "displayGradeAs": "Percentage",
+        "assignmentGroup": "ASSIGNMENTS",
+        "mediaOptions": {
+            "textEntry": false,
+            "websiteUrl": true,
+            "mediaRecordings": false,
+            "studentAnnotation": false,
+            "fileUpload": false
+        }
+    },
+    {
+        "_id": "A102",
+        "title": "Combustion Analysis",
+        "course": "RS101",
+        "description": "Combustion Analysis placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "Online",
+        "displayGradeAs": "Points",
+        "assignmentGroup": "PROJECTS",
+        "mediaOptions": {
+            "textEntry": false,
+            "websiteUrl": true,
+            "mediaRecordings": true,
+            "studentAnnotation": false,
+            "fileUpload": false
+        }
+    },
+    {
+        "_id": "A103",
+        "title": "Nozzle Design Project",
+        "course": "RS101",
+        "description": "Nozzle Design Project placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "Online",
+        "displayGradeAs": "Percentage",
+        "assignmentGroup": "PROJECTS",
+        "mediaOptions": {
+            "textEntry": true,
+            "websiteUrl": true,
+            "mediaRecordings": false,
+            "studentAnnotation": false,
+            "fileUpload": true
+        }
+    },
+    {
+        "_id": "A201",
+        "title": "Aerodynamics Quiz",
+        "course": "RS102",
+        "description": "Aerodynamics Quiz placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "In-Person",
+        "displayGradeAs": "Points",
+        "assignmentGroup": "QUIZZES",
+        "mediaOptions": {
+            "textEntry": false,
+            "websiteUrl": false,
+            "mediaRecordings": false,
+            "studentAnnotation": false,
+            "fileUpload": false
+        }
+    },
+    {
+        "_id": "A202",
+        "title": "Flow Analysis",
+        "course": "RS102",
+        "description": "Flow Analysis placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "Online",
+        "displayGradeAs": "Percentage",
+        "assignmentGroup": "ASSIGNMENTS",
+        "mediaOptions": {
+            "textEntry": false,
+            "websiteUrl": true,
+            "mediaRecordings": true,
+            "studentAnnotation": false,
+            "fileUpload": true
+        }
+    },
+    {
+        "_id": "A203",
+        "title": "Heating Analysis",
+        "course": "RS102",
+        "description": "Heating Analysis placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "Online",
+        "displayGradeAs": "Points",
+        "assignmentGroup": "ASSIGNMENTS",
+        "mediaOptions": {
+            "textEntry": false,
+            "websiteUrl": true,
+            "mediaRecordings": false,
+            "studentAnnotation": true,
+            "fileUpload": true
+        }
+    },
+    {
+        "_id": "A301",
+        "title": "Structural Design Task",
+        "course": "RS103",
+        "description": "Structural Design Task placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "Online",
+        "displayGradeAs": "Percentage",
+        "assignmentGroup": "PROJECTS",
+        "mediaOptions": {
+            "textEntry": true,
+            "websiteUrl": true,
+            "mediaRecordings": false,
+            "studentAnnotation": false,
+            "fileUpload": true
+        }
+    },
+    {
+        "_id": "A302",
+        "title": "Orbital Calculations",
+        "course": "RS103",
+        "description": "Orbital Calculations placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "In-Person",
+        "displayGradeAs": "Points",
+        "assignmentGroup": "QUIZZES",
+        "mediaOptions": {
+            "textEntry": false,
+            "websiteUrl": false,
+            "mediaRecordings": false,
+            "studentAnnotation": false,
+            "fileUpload": false
+        }
+    },
+    {
+        "_id": "A303",
+        "title": "Systems Engineering Exam",
+        "course": "RS103",
+        "description": "Systems Engineering Exam placeholder",
+        "points": 100,
+        "dueDate": "2024-05-13",
+        "availableFrom": "2024-05-06",
+        "availableUntil": "2024-05-20",
+        "assignTo": "Everyone",
+        "submissionType": "Online",
+        "displayGradeAs": "Percentage",
+        "assignmentGroup": "QUIZZES",
+        "mediaOptions": {
+            "textEntry": false,
+            "websiteUrl": true,
+            "mediaRecordings": false,
+            "studentAnnotation": true,
+            "fileUpload": true
+        }
+    }
+]
+
