995673177ffa28206a66385776950ff4e3d71cf2
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-11-24 13:17:58 -0800

    finish 5.2

--- kanbas/modules/dao.js
+++ kanbas/modules/dao.js
@@ -1,26 +1,32 @@
 import Database from "../database/index.js"
+import model from "./model.js"
 
 export const findModulesForCourse = (courseId) => {
-    const { modules } = Database
-    return modules.filter((module) => module.course === courseId)
+    return model.find({ course: courseId })
+    // const { modules } = Database
+    // return modules.filter((module) => module.course === courseId)
 }
 
 export const createModule = (module) => {
-    const newModule = { ...module, _id: Date.now().toString() }
-    Database.modules = [...Database.modules, newModule]
-    return newModule
+    delete module._id
+    return model.create(module)
+    // const newModule = { ...module, _id: Date.now().toString() }
+    // Database.modules = [...Database.modules, newModule]
+    // return newModule
 }
 
 export const deleteModule = (moduleId) => {
-    const { modules } = Database
-    const initialLength = modules.length
-    Database.modules = modules.filter((module) => module._id !== moduleId)
-    return Database.modules.length < initialLength
+    return model.deleteOne({ _id: moduleId })
+    // const { modules } = Database
+    // const initialLength = modules.length
+    // Database.modules = modules.filter((module) => module._id !== moduleId)
+    // return Database.modules.length < initialLength
 }
 
 export const updateModule = (moduleId, moduleUpdates) => {
-    const { modules } = Database
-    const module = modules.find((module) => module._id === moduleId)
-    Object.assign(module, moduleUpdates)
-    return module
+    return model.updateOne({ _id: moduleId }, moduleUpdates)
+    // const { modules } = Database
+    // const module = modules.find((module) => module._id === moduleId)
+    // Object.assign(module, moduleUpdates)
+    // return module
 }
