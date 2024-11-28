e0c2a8e732ebd5a5ce50b9ea7311fa31967b92ee
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-11-27 18:41:21 -0800

    fix

--- kanbas/quizzes/routes.js
+++ kanbas/quizzes/routes.js
@@ -44,7 +44,7 @@ export const QuizRoutes = (app) => {
     });
 
     //get published quizzes
-    app.get("/api/courses/:cid/quizzes/published", async (req, res) => {
+    app.get("/api/courses/:cid/quizzes/", async (req, res) => {
         const { cid } = req.params
         const publishedQuizzes = await dao.getPublishedQuizzesByCourse(cid)
         if (!publishedQuizzes) {
